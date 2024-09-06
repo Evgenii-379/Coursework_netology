dd05bbd24bd9cb4a41cd122e5d6357f043fc22d6
Evgenii-379 <152536714+Evgenii-379@users.noreply.github.com>
2024-09-06 16:47:41 +0400

    Create target.group.tf

---
+++ terraform/target.group.tf
@@ -0,0 +1,13 @@
+resource "yandex_alb_target_group" "target-group" {
+  name     = "my-target-group"
+ 
+  target {
+    subnet_id = "e9bpt4uvnnr7ji9j2t6b"    #subnet 1
+    ip_address = "10.0.1.0"
+}
+ 
+  target {
+    subnet_id = "e2ldt0p3arsqhpcsivks"    #subnet 2   
+    ip_address = "10.0.2.0"
+ }
+}
