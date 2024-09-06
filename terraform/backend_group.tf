3b0c76fcf38f44cac345041282f5ff0f0c5eb465
Evgenii-379 <152536714+Evgenii-379@users.noreply.github.com>
2024-09-06 16:43:53 +0400

    Create backend_group.tf

---
+++ terraform/backend_group.tf
@@ -0,0 +1,24 @@
+#Создание Backend Group:
+ 
+ resource "yandex_alb_backend_group" "my_backend_group" {
+  name = "my-backend-group"
+ 
+  http_backend {
+    name             = "http-backend"
+    weight           = 1
+    port             = 80
+    target_group_ids = [yandex_alb_target_group.target-group.id]
+ 
+    load_balancing_config {
+      panic_threshold = 90
+    }
+ 
+    healthcheck {
+      timeout  = "5s"
+      interval = "1s"
+      http_healthcheck {
+        path = "/"
+     }
+    }
+  }
+}
